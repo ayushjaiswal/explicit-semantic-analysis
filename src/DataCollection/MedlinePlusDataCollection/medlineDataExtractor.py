077cba062df72f2c311a646c8703a8a0ff7a0324
bhargava.anunay <bhargava.anunay@1f02d34c-50eb-ad8c-e7b6-fa19dcba30ef>
2013-09-22 20:08:32 +0000

    Medline Data extracted

--- src/DataCollection/MedlinePlusDataCollection/medlineDataExtractor.py
+++ src/DataCollection/MedlinePlusDataCollection/medlineDataExtractor.py
@@ -1,4 +1,5 @@
 import workerpool
+import re
 import os
 import urllib2
 from bs4 import BeautifulSoup
@@ -6,11 +7,16 @@ from bs4 import BeautifulSoup
 class MedlineDataExtractor(workerpool.Job):
     """Fetches the data from the given link and save"""
     
-    def __init__(self, url, count, isArticle=False):
+    def __init__(self, url, isArticle=False):
         self.__url = url
-        self.__count = count
-        self.__path = "../../../data/Medline/"
+        self.__topicPath = "../../../data/Medline/Topics/"
+        self.__articlePath = "../../../data/Medline/Articles/"
         self.__isArticle = isArticle
+        if not os.path.exists(self.__articlePath):
+            os.makedirs(self.__articlePath)
+        if not os.path.exists(self.__topicPath):
+            os.makedirs(self.__topicPath)
+        
 
     def __fetchHtml(self):
         response = urllib2.urlopen(self.__url)
@@ -18,36 +24,41 @@ class MedlineDataExtractor(workerpool.Job):
         response.close()
         return html_doc
         
-    def __saveTopic(self, soupObj, fileName):
+
+    def __saveTopic(self, soupObj):
         """Fetches and saves data from different topics"""
         
         title = soupObj.find("h1", {"id":"tp_title"})
         alsoKnown = soupObj.find("div", {"id":"ht_alsoknown"})
         body = soupObj.find("div", {"id":"tpsummary"})
         if not title is None and not body is None:
-            titleText = title.text
+            titleText = title.text.strip().encode('utf-8')
+            fileName = re.sub(' +', ' ', titleText)
+            fileName = re.sub('_+', ' ', fileName)
+            fileName = re.sub('/+', ' ', fileName)
             bodyText = body.text
             text = "Title: " + titleText + '\n\n'
             if not alsoKnown is None:
                 text = text + alsoKnown + '\n\n'
             text = text + bodyText
-            self.__count = self.__count + 1
-            with open(self.__path + fileName,'w') as f:
+            text = text + '\n\n'+'Link of medline: '+self.__url
+            with open(self.__topicPath + fileName,'w') as f:
                 f.write(text.encode('utf-8'))
+            print 'Downloaded topic : %s'%fileName
     
+
     def __saveArticle(self, soupObj):
         """Fetches and saves data from different articles"""
         
         title = soupObj.find("h1", {"class":"pheader"})
         body = soupObj.find("div", {"id":"encymain"})
         if not title is None and not body is None:
-            titleText = title.text
+            titleText = title.text.strip()
             bodyContents = body.div.div
-            self.__count = self.__count + 1
             fileName = titleText.encode('utf-8')
-            fileName = fileName.replace('/', '_')
-            fileName = fileName.replace(' ', '_')
-            fileName = fileName.replace('-', '_')
+            fileName = re.sub('/+', ' ',fileName)
+            fileName = re.sub('_+', ' ',fileName)
+            fileName = re.sub(' +', ' ',fileName)
             text = 'Title: ' + titleText + '\n\n'
             while not bodyContents is None:
                 if bodyContents.text == 'References':
@@ -57,12 +68,13 @@ class MedlineDataExtractor(workerpool.Job):
                 else:
                     text = text + bodyContents.text
                 bodyContents = bodyContents.nextSibling
+            text = text + '\n\n'+'Link of medline: '+self.__url
             text = text.encode('utf-8')
-            with open(self.__path + fileName, 'w') as f:
+            with open(self.__articlePath + fileName, 'w') as f:
                 f.write(text)
-        else:
-            print self.__url
-             
+            print 'Downloaded article : %s'%fileName
+                                    
+
     def run(self):
         soup = BeautifulSoup(self.__fetchHtml())
         link = self.__url[::-1]
@@ -71,5 +83,4 @@ class MedlineDataExtractor(workerpool.Job):
         if self.__isArticle:
             self.__saveArticle(soup)
         else:
-            fileName = self.__url[lenUrl - idx1 : lenUrl-5]        
-            self.__saveTopic(soup, fileName)
+            self.__saveTopic(soup)
--- src/DataCollection/MedlinePlusDataCollection/medlineRunner.py
+++ src/DataCollection/MedlinePlusDataCollection/medlineRunner.py
@@ -1,23 +1,44 @@
 from medlineLinkExtractor import MedlineLinkExtractor
 from medlineDataExtractor import MedlineDataExtractor
+from os import listdir
+from os.path import isfile, join
 import workerpool
 
+#extract topics and articles url
 mLE = MedlineLinkExtractor()
 mLE.readXml("../../../etc/mplus_topics_2013-09-14.xml")
 topics = mLE.getTopics()
 articles = mLE.getArticles()
-count = 0
+topicCount = 0
+articleCount = 0
         
-pool = workerpool.WorkerPool(size=100)
-for url in articles:
-    count = count + 1
-    job = MedlineDataExtractor(url, count, isArticle=True)
-    pool.put(job)
-
+#Downloader
+pool = workerpool.WorkerPool(size=20)
 for url in topics:
-    count = count + 1
-    job = MedlineDataExtractor(url, count, isArticle=False)
+    topicCount = topicCount + 1
+    job = MedlineDataExtractor(url, isArticle=False)
     pool.put(job)
     
+for url in articles:
+    articleCount = articleCount + 1
+    job = MedlineDataExtractor(url, isArticle=True)
+    pool.put(job)
+
 pool.shutdown()
 pool.wait()
+print str(topicCount) + ' Topics downloaded'
+print str(articleCount) + ' Articles downloaded'
+
+#save the index files for aricles and topics 
+topicPath = '../../../data/Medline/Topics/'
+articlePath = '../../../data/Medline/Articles/'
+indexFilePath = '../../../data/Medline/'
+topicFiles = [fileName for fileName in listdir(topicPath) if isfile(topicPath+fileName)]
+articleFiles = [fileName for fileName in listdir(articlePath) if isfile(articlePath+fileName)]
+with open(indexFilePath+'Topics Index','w') as f:
+    for fileName in topicFiles:
+        f.write(fileName + '\n')
+
+with open(indexFilePath+'Article Index','w') as f:
+    for fileName in articleFiles:
+        f.write(fileName + '\n')
