45f39953dad83a97e3536d0eeab53793197218cd
mail.ayush.jaiswal <mail.ayush.jaiswal@1f02d34c-50eb-ad8c-e7b6-fa19dcba30ef>
2013-10-26 09:12:20 +0000

    Added application name to configuration.

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 from SemanticRelatedness.srcRunner import SRC_Runner
 
 ApplicationRunner = SRC_Runner
+ApplicationName = 'Semantic Relatedness Calculator'
 ESADumpPath = '../data/ESAModel/'
 ESADumpFileName = 'TFIDF_InvertedIndex.pkl'
--- src/runner.py
+++ src/runner.py
@@ -5,15 +5,16 @@ import ESA_util
 class Runner:
     """Creates a platform for running an ESA-based application program, and then runs it."""
 
-    def __init__(self, ESADumpPath, ESADumpFileName, ApplicationRunner):
+    def __init__(self, ESADumpPath, ESADumpFileName, ApplicationRunner, ApplicationName):
         loader = ObjectDumperAndLoader()
         self.__ESADump = loader.load(ESADumpPath, ESADumpFileName)
         self.__app = ApplicationRunner(self.__ESADump)
+        self.__appName = ApplicationName
 
     def run(self):
         """Runs the application."""
 
-        print "Application started!"
+        print self.__appName, "started!"
         shouldExit = 'n'
         while shouldExit != 'y':
             self.__app.run()
@@ -21,7 +22,7 @@ class Runner:
         print "Bye!"
 
 def main():
-    runnerObj = Runner(config.ESADumpPath, config.ESADumpFileName, config.ApplicationRunner)
+    runnerObj = Runner(config.ESADumpPath, config.ESADumpFileName, config.ApplicationRunner, config.ApplicationName)
     runnerObj.run()
 
 if __name__ == '__main__':
