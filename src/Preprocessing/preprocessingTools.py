f33ebc36e48d3f164826d653ad0b6f6d4918b9ad
mail.ayush.jaiswal <mail.ayush.jaiswal@1f02d34c-50eb-ad8c-e7b6-fa19dcba30ef>
2014-03-18 19:06:26 +0000

    Fixed lemmatization.

--- src/Preprocessing/preprocessingTools.py
+++ src/Preprocessing/preprocessingTools.py
@@ -3,6 +3,7 @@
 # Here, we use the implementations provided by NLTK.
 
 import nltk
+from collections import defaultdict
 
 class Preprocessor:
     """Preprocesses the raw text for further operations.
@@ -39,16 +40,22 @@ class Preprocessor:
         porter = nltk.PorterStemmer()
         return [porter.stem(t) for t in tokens]
 
-    def __lemmatize(self, tokens):
-        """Performs stemming on tokens and returns the list of lemmas."""
+    def __lemmatize(self, tagged_tokens):
+        """Performs lemmatization on POS tagged tokens and returns the list of lemmas."""
 
         wnl = nltk.WordNetLemmatizer()
-        return [wnl.lemmatize(t) for t in tokens]
+        pos_map = defaultdict(lambda: 'n', {'N' : 'n', 'J' : 'a', 'V' : 'v', 'R' : 'r'})
+        return [wnl.lemmatize(token, pos_map[pos_tag[0]]) for token, pos_tag in tagged_tokens]
 
     def __isNotPunctuation(self, x):
         """Returns True if x is a punctuation mark."""
 
         return ((len(x) > 1) or x.isalnum())
+
+    def __POSTag(self, tokens):
+        """Performs Parts of Speech Tagging on the tokens and returns list of (token, tag)."""
+
+        return nltk.pos_tag(tokens)
 
     def __filterPunctuation(self, tokens):
         """Removes all punctuation marks from tokens."""
@@ -83,18 +90,19 @@ class Preprocessor:
 
         """
 
-        rawTokens = [t.lower() for t in self.__tokenize(text)]
-        if self.__shouldFilterStopWords:
-            rawTokens = self.__filterStopWords(rawTokens)
-        stemmedTokens = self.__stem(rawTokens)
-        lemmatizedTokens = self.__lemmatize(rawTokens)
+        tokens = []
         self.TokenType = Preprocessor.TokenType
         if type == self.TokenType.stemmed:
-            tokens = stemmedTokens
+            tokens = [t.lower() for t in self.__tokenize(text)]
+            tokens = self.__stem(tokens)
         elif type == self.TokenType.lemmatized:
-            tokens = lemmatizedTokens
+            taggedTokens = self.__POSTag(self.__tokenize(text))
+            tokens = self.__lemmatize(taggedTokens)
         else:
-            tokens = self.__rawTokens
+            tokens = [t.lower() for t in self.__tokenize(text)]
+
+        if self.__shouldFilterStopWords:
+            tokens = self.__filterStopWords(tokens)
 
         if self.__shouldFilterPunctuation:
             return self.__filterPunctuation(tokens)
